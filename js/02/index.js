285f22db1d6b2d117de4ee420a0b876e15097ff2
MariaDemianchuk <106021153+MariaDemianchuk@users.noreply.github.com>
2022-07-25 09:14:25 +0300

    ADD HW 3 JS

--- js/02/index.js
+++ js/02/index.js
@@ -62,4 +62,60 @@ console.log(2 || 0 || 3, 'повертає вираз 2 || 0 || 3');
 // 2 && 0 || 3 - 3, послідовність дій зліва на право - перший оператор (логічне "і") повертає 0, далі другий оператор (логічне "або") із 0 та 3 вибирає перше true і відповідно повертає 3
 console.log(2 && 0 || 3,'повертає вираз 2 && 0 || 3' );
 
+let month = prompt('введіть номер місяця');
+if (Number(month)>0 && Number(month)<=12) {
+    switch (month){
+        case '1':
+            alert ('січень')
+        break
+        case '2':
+            alert ('лютий')
+        break
+        case '3':
+            alert ('березень')
+        break
+        case '4':
+            alert ('квітень')
+        break
+        case '5':
+            alert ('травень')
+        break
+        case '6':
+            alert ('червень')
+        break
+        case '7':
+            alert ('липень')
+        break
+        case '8':
+            alert ('серпень')
+        break
+        case '9':
+            alert ('вересень')
+        break
+        case '10':
+            alert ('жовтень')
+        break
+        case '11':
+            alert ('листопад')
+        break
+        case '12':
+            alert ('грудень')
+        break
+    } 
+} else {
+    alert ('введіть вірний номер місяця')
+}
+
+let sheepQuantity = prompt ('enter number of sheep')
+let sheepsText = '';
+if (sheepQuantity>0) {
+    for (i=1; i<=sheepQuantity; i++){
+
+       sheepsText += `${i} sheeps...`
+    }
+} else {
+    alert ("enter valid value")
+}
+alert (sheepsText)
+
 
