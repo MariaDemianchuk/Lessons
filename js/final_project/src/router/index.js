57630b60b2b5a2ea12000b87a21b6fefbca05069
MariaDemianchuk <106021153+MariaDemianchuk@users.noreply.github.com>
2022-09-17 15:16:44 +0300

    ADDED final project start

---
+++ js/final_project/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import StartPage from "../views/StartPage.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: StartPage,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
