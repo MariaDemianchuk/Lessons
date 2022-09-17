0390ad28baaf8bb0e05671c8dbea748e4b4fd5f1
MariaDemianchuk <106021153+MariaDemianchuk@users.noreply.github.com>
2022-09-17 21:08:18 +0300

    add body start

--- js/final_project/src/views/start_page/HeaderStart.vue
+++ js/final_project/src/views/start_page/HeaderStart.vue
@@ -1,15 +1,10 @@
 <template>
-  <div>
-    <b-navbar variant="faded" type="light">
-      <b-navbar-brand href="#">
-        <img
-          src="../../assets/logo.png"
-          class="d-inline-block align-top"
-          alt="logo"
-        />
-        BootstrapVue
-      </b-navbar-brand>
-    </b-navbar>
+  <div class="header">
+    <img src="../../assets/logo.png" alt="logo" />
+    <div class="buttons">
+      <button>Вхід</button>
+      <button>Реєстрація</button>
+    </div>
   </div>
 </template>
 
@@ -19,4 +14,32 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap");
+.header {
+  display: flex;
+  justify-content: space-between;
+  max-height: 80px;
+  border-bottom: 1px solid rgb(198, 195, 195);
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px,
+    rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
+  img {
+    height: 80px;
+  }
+  .buttons {
+    display: flex;
+    justify-content: space-between;
+    width: 250px;
+    padding: 10px;
+
+    button {
+      width: 110px;
+      color: white;
+      background-color: black;
+      font-family: "Montserrat", sans-serif;
+      border: none;
+      border-radius: 10px;
+    }
+  }
+}
+</style>
--- js/final_project/src/views/start_page/StartPage.vue
+++ js/final_project/src/views/start_page/StartPage.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <HeaderStart />
+    <BodyStart />
   </div>
 </template>
 
 <script>
 import HeaderStart from "./HeaderStart.vue";
+import BodyStart from "./BodyStart.vue";
+
 export default {
   name: "StartPage",
   components: {
     HeaderStart,
+    BodyStart,
   },
 };
 </script>
