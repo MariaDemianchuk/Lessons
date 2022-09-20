bcd191394b8946a8e53132b3bcfc16c7c6cc250b
MariaDemianchuk <106021153+MariaDemianchuk@users.noreply.github.com>
2022-09-20 22:38:49 +0300

    change axios into fetch in auth.js

--- js/final_project/src/store/modules/auth.js
+++ js/final_project/src/store/modules/auth.js
@@ -1,5 +1,4 @@
-import axios from "axios";
-axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+// import axios from "axios";
 
 const state = {
   name: "Maria Riaba",
@@ -9,14 +8,18 @@ const mutations = {};
 const actions = {
   async signup(context, payload) {
     let postData = {
-      email: payload.email,
-      password: payload.password,
-      returnSecureToken: true,
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
     };
-    let response = await axios.post(
+    let response = fetch(
       "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCAjYh5sbL-7MSQ7ISkqp8IePcM8JN4B1Y",
       postData
-    );
+    ).then((response) => response.json());
     console.log(response);
   },
 };
